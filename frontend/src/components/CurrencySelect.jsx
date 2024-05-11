0d31edc3a94bc180444adde38d7e2c6a5c49d09b
aleksei-shvetz <al.sv.dev@gmail.com>
2024-05-11 11:46:54 +0300

    Correct cursorSelect

--- frontend/src/components/CurrencySelect.jsx
+++ frontend/src/components/CurrencySelect.jsx
@@ -1,32 +1,55 @@
 import Form from 'react-bootstrap/Form';
-// import { useEffect } from 'react';
+import Spinner from 'react-bootstrap/Spinner';
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import currencies from '../constatnts/currencies.js';
-import { latestThank, updateBaseCurrency } from '../store/ratesSlice.js';
-import ratesSelectors from '../store/selectors.js';
+import { latestThank } from '../store/ratesSlice.js';
+import useBaseCurrency from '../contexts/hooks/useBaseCurrency.js';
+import { ratesSelectors } from '../store/selectors.js';
 
 const CurrecySelect = () => {
-  const base = useSelector(ratesSelectors.getBaseCurrency);
+  const currencyHook = useBaseCurrency();
+  const currentBase = currencyHook.base;
   const dispatch = useDispatch();
+  const [baseCurrency, setBaseCurrency] = useState(currentBase);
+  const loadingStatus = useSelector(ratesSelectors.getloadingStatus);
 
   const selectHandle = async (event) => {
-    dispatch(updateBaseCurrency(event.target.value));
-    await dispatch(latestThank(event.target.value));
+    if (event.target.value !== currentBase) {
+      currencyHook.updateBaseCurrency(event.target.value);
+      setBaseCurrency(event.target.value);
+      await dispatch(latestThank(event.target.value));
+    }
   };
 
-  console.log(base);
+  const renderSpinner = () => (
+    <div>
+      <Spinner
+        as="span"
+        animation="border"
+        size="sm"
+        role="status"
+        aria-hidden="true"
+        className="me-2"
+      />
+      <span className="visually-hidden">Loading...</span>
+    </div>
+  );
 
   return (
-    <Form.Select
-      value={useSelector(ratesSelectors.getBaseCurrency)}
-      aria-label="Default select example"
-      size="sm"
-      onChange={selectHandle}
-    >
-      {currencies.map((currency) => (
-        <option key={currency} id={currency} value={currency}>{currency}</option>
-      ))}
-    </Form.Select>
+    <div className="d-flex justify-content-center align-items-center">
+      {loadingStatus === 'loading' ? renderSpinner() : null}
+      <Form.Select
+        value={baseCurrency}
+        aria-label="Default select example"
+        size="sm"
+        onChange={selectHandle}
+      >
+        {currencies.map((currency) => (
+          <option key={currency} id={currency} value={currency}>{currency}</option>
+        ))}
+      </Form.Select>
+    </div>
   );
 };
 
