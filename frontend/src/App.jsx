31524138fc2ac681538e1407a17717b14f4abee1
aleksei-shvetz <al.sv.dev@gmail.com>
2024-05-11 11:40:20 +0300

    Correct TopNav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable react/jsx-filename-extension */
-/* eslint-disable react/react-in-jsx-scope */
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import {
@@ -9,10 +6,11 @@ import {
 import ROUTES from './pages/route.js';
 import Home from './pages/Home.jsx';
 import Converter from './pages/Converter.jsx';
-// import TabProvider from './providers/tabProvider.js';
+import TopNav from './components/TopNav.jsx';
 
 const App = () => (
   <BrowserRouter>
+    <TopNav />
     <Routes>
       <Route path={ROUTES.currencyList} element={<Home />} />
       <Route path={ROUTES.converter} element={<Converter />} />
--- frontend/src/components/TopNav.jsx
+++ frontend/src/components/TopNav.jsx
@@ -1,18 +1,48 @@
 import Nav from 'react-bootstrap/Nav';
 // import Navbar from 'react-bootstrap/Navbar';
 import { useTranslation } from 'react-i18next';
+import { useSelector, useDispatch } from 'react-redux';
 // import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import ROUTES from '../pages/route.js';
-// import useTabHook from '../contexts/hooks/tabHook.js';
+import { ratesSelectors } from '../store/selectors.js';
+import { show } from '../store/modalSlice.js';
+
 // import Container from '../containers/mainContainer.jsx';
 import CurrecySelect from './CurrencySelect.jsx';
+import useBaseCurrency from '../contexts/hooks/useBaseCurrency.js';
+import { latestThank } from '../store/ratesSlice.js';
+import ErrorModal from './ErrorModal.jsx';
 
 const TopNav = () => {
+  // const isShowModal = useSelector(modalSelectors.getModalStatus);
+  const dispatch = useDispatch();
+  const currencyHook = useBaseCurrency();
   const { t } = useTranslation();
   const [activPage, setActivePage] = useState('/');
   const { pathname } = useLocation();
+  const err = useSelector(ratesSelectors.getErr);
+
+  const getErrModal = () => {
+    if (err === '429') {
+      dispatch(show());
+      return <ErrorModal />;
+    }
+    return null;
+  };
+
+  const rates = useSelector(ratesSelectors.getRates);
+
+  useEffect(() => {
+    const currentBase = currencyHook.base;
+    const fetchData = async () => {
+      await dispatch(latestThank(currentBase));
+    };
+    if (Object.keys(rates).length === 0) {
+      fetchData();
+    }
+  }, []);
 
   useEffect(() => {
     switch (pathname) {
@@ -28,23 +58,26 @@ const TopNav = () => {
   }, [activPage]);
 
   return (
-    <Nav
-      variant="underline"
-      activeKey={activPage}
-      className="bg-light p-4 d-flex flex-row align-items-center sticky-top"
-    >
-      <div className="d-flex flex-row align-items-center flex-grow-1">
-        <Nav.Item className="me-3">
-          <Nav.Link className="text-secondary" id="currencyList" eventKey="/" href={ROUTES.currencyList}>{t('headers.currencies')}</Nav.Link>
+    <>
+      <Nav
+        variant="underline"
+        activeKey={activPage}
+        className="bg-light p-4 d-flex flex-row align-items-center sticky-top mb-3"
+      >
+        <div className="d-flex flex-row align-items-center flex-grow-1">
+          <Nav.Item className="me-3">
+            <Nav.Link className="text-secondary" id="currencyList" eventKey="/" href={ROUTES.currencyList}>{t('headers.currencies')}</Nav.Link>
+          </Nav.Item>
+          <Nav.Item className="me-3">
+            <Nav.Link className="text-secondary" id="converter" eventKey="converter" href={ROUTES.converter}>{t('headers.converter')}</Nav.Link>
+          </Nav.Item>
+        </div>
+        <Nav.Item>
+          <CurrecySelect />
         </Nav.Item>
-        <Nav.Item className="me-3">
-          <Nav.Link className="text-secondary" id="converter" eventKey="converter" href={ROUTES.converter}>{t('headers.converter')}</Nav.Link>
-        </Nav.Item>
-      </div>
-      <Nav.Item>
-        <CurrecySelect />
-      </Nav.Item>
-    </Nav>
+      </Nav>
+      {err ? getErrModal() : null}
+    </>
   );
 };
 
