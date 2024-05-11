c84ab96e939448ad388a9b39a5fc8e8cbc35992d
aleksei-shvetz <al.sv.dev@gmail.com>
2024-05-12 01:08:04 +0300

    Rework Home

--- frontend/src/components/CurrencyRateItem.jsx
+++ frontend/src/components/CurrencyRateItem.jsx
@@ -1,16 +1,28 @@
+const calculate = (rate) => Math.round(100 * Number(rate)) / 100;
+
 const CurrencyRateItem = ({
   baseCurrency, currencyCode, fullName, rate,
 }) => (
   <div className="d-flex flex-row align-items-center border-bottom mt-3">
     <div className="d-flex flex-row align-items-center flex-grow-1">
+      <div className="me-2">{`1 ${baseCurrency}`}</div>
+      <div className="me-2">=</div>
+      <div className="me-2">{calculate(rate)}</div>
+      <div className="me-2">{currencyCode}</div>
+    </div>
+    <div className="small d-flex flex-row align-items-center">
       <div className="me-2">
         <strong>
-          {`${baseCurrency} / ${currencyCode}`}
+          {`${currencyCode}`}
         </strong>
       </div>
-      <div>{`(${fullName})`}</div>
+      <div className="me-2">{`(${fullName})`}</div>
+      <div className="">
+        <strong>
+          {rate}
+        </strong>
+      </div>
     </div>
-    <div className="">{rate}</div>
   </div>
 );
 
