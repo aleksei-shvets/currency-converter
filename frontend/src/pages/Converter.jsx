7e34830bf7a25c65e5ae34696c12b4a72e336b65
aleksei-shvetz <al.sv.dev@gmail.com>
2024-05-11 11:47:33 +0300

    Rework Converter

--- frontend/src/pages/Converter.jsx
+++ frontend/src/pages/Converter.jsx
@@ -1,13 +1,69 @@
-import TopNav from '../components/TopNav.jsx';
-// import Container from '../containers/mainContainer.jsx';
+/* eslint-disable consistent-return */
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { ratesSelectors } from '../store/selectors.js';
+import FromConvertSelect from '../components/FromConvertInput.jsx';
+import ToConvertSelect from '../components/ToConvertInput.jsx';
+import state from '../store/index.js';
 
-const Converter = () => (
-  <>
-    <TopNav currentTab="converter" />
-    <div>
-      this is converter
+const Converter = () => {
+  const [rates, setRates] = useState({});
+  const stateRates = useSelector(ratesSelectors.getRates);
+  const currentBase = useSelector(ratesSelectors.getBaseCurrency);
+  const [fromCurrency, setFromCurrency] = useState(currentBase);
+  const [toCurrency, setToCurrency] = useState(currentBase);
+  const [countConvertFrom, setCountConvertFrom] = useState(1);
+
+  useEffect(() => {
+    console.log(state.getState());
+    setRates(stateRates);
+    console.log(rates);
+  }, [stateRates]);
+
+  const convertWithBaseCurrency = () => {
+    const rate = rates[toCurrency];
+    return Number(countConvertFrom) * Number(rate);
+  };
+
+  const convertDifferntCurrencies = () => {
+    const fromRate = rates[fromCurrency];
+    const toRate = rates[toCurrency];
+    console.log((Number(countConvertFrom) * Number(fromRate)) * Number(toRate));
+    return (Number(countConvertFrom) * Number(fromRate)) * Number(toRate);
+  };
+
+  const convertCountTo = () => {
+    if (Object.keys(rates).length !== 0) {
+      if (currentBase === fromCurrency) {
+        return convertWithBaseCurrency();
+      }
+      return convertDifferntCurrencies();
+    }
+  };
+
+  return (
+    <div className="d-flex flex-row justify-content-center">
+      <div
+        className="d-inline-flex flex-row justify-content-center flex-wrap rounded p-3 bg-light"
+      >
+        <div>
+          <FromConvertSelect
+            selectedCurrency={fromCurrency}
+            inputValueFrom={countConvertFrom}
+            inputHandleFrom={setCountConvertFrom}
+            handlerFromCurrency={setFromCurrency}
+          />
+        </div>
+        <div>
+          <ToConvertSelect
+            inputHandleTo={convertCountTo}
+            handlerFromCurrency={setToCurrency}
+            selectedCurrency={toCurrency}
+          />
+        </div>
+      </div>
     </div>
-  </>
-);
+  );
+};
 
 export default Converter;
