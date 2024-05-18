0527853f42576be773e2fd3966614a0a9e1bf6dd
aleksei-shvetz <al.sv.dev@gmail.com>
2024-05-18 23:53:28 +0300

    Add getUrlConvert

--- frontend/src/fetchApi/fetchApi.js
+++ frontend/src/fetchApi/fetchApi.js
@@ -1,14 +1,26 @@
 const baseUrl = 'https://api.freecurrencyapi.com/v1/';
+const apiPath = 'latest';
 
 const getUrlOfBaseCurrency = (baseCurrency) => {
-  const apiUrl = new URL('latest', baseUrl);
-  const params = apiUrl.searchParams;
+  const url = new URL(apiPath, baseUrl);
+  const params = url.searchParams;
   params.set('base_currency', baseCurrency);
-  return apiUrl.href;
+  return url.href;
 };
+
+const getUrlConvert = (baseCurrency, toCurrency) => {
+  const url = new URL(apiPath, baseUrl);
+  const params = url.searchParams;
+  params.set('currencies', toCurrency);
+  params.set('base_currency', baseCurrency);
+  return url.href;
+};
+
+// https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_a1nKOeuDHWsJZKuaIYAdmoFrGvbVF9R6ccBN1Am3&currencies=EUR&base_currency=BGN
 
 const getFetchUrl = {
   latest: (baseCurrency) => getUrlOfBaseCurrency(baseCurrency),
+  convert: (baseCurrency, toCurrency) => getUrlConvert(baseCurrency, toCurrency),
 };
 
 export default getFetchUrl;
